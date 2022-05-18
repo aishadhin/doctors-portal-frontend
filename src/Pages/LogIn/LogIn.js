c238ed8577a643fcac2248ec1de5b8ac546c2d46
Ai Shadhin <aishadhin.ac@gmail.com>
2022-05-18 12:27:47 +0600

    google signIn configure

--- src/Pages/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn.js
@@ -1,11 +1,29 @@
-import React from 'react';
-
+import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 const LogIn = () => {
-    return (
-        <div>
-            <h2>This is Log in</h2>
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+  if(user){
+    console.log(user);
+  }
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title justify-center">Login</h2>
+
+          <div class="divider">OR</div>
+          <button
+            onClick={() => signInWithGoogle()}
+            class="btn btn-outline bg-primary text-white"
+          >
+            Continue With Google
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
